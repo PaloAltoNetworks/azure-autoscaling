c4b89f1381f83d281971028bd181e729c4101b20
Narayan Iyengar <niyengar@paloaltonetworks.com>
2018-04-06 17:02:49 -0700

    commit message

--- node_woker.py
+++ node_woker.py
@@ -2,7 +2,8 @@
 
 import json
 import subprocess
-from bottle import run, post, request, response, get, route
+from bottle import request, run, route
+from bottledaemon import daemon_run
 import shlex
 import urllib2
 import xml.etree.ElementTree as et
@@ -11,6 +12,8 @@ import socket
 import time
 import logging
 import sys
+import collections
+import itertools
 
 #TO DO
 #1. this script assume az login has been done apriori
@@ -20,13 +23,17 @@ import sys
 #   https://docs.microsoft.com/en-us/azure/monitoring-and-diagnostics/monitoring-enable-alerts-using-template
 #   Seems like there might be a way to do custom metrics
 #   https://github.com/F5Networks/f5-azure-arm-templates/blob/master/supported/solutions/autoscale/waf/existing_stack/PAYG/azuredeploy.json
+#3. The worker node can be launched as part of the template with a custom script extension to launch the script
+#   So in this case can VMSS notification URL be http://{ref privatr ip}
 
 LOG_FILENAME = 'azure-autoscaling.log'
 logging.basicConfig(filename=LOG_FILENAME,level=logging.INFO, filemode='w',format='%(message)s',)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-instance_id = list()
+
+instance_list = collections.defaultdict(list)
+instance_id = ""
 fw_ip = list()
 fw_untrust_ip = list()
 scaled_fw_ip = "1.1.1.1"
@@ -170,18 +177,19 @@ def check_job_status(ip_to_monitor, job_id):
                         else:
                             return 'pending'
 
-#@route('/', method='POST')
-#def process():
-def main():
+@route('/', method='POST')
+def index():
     global fw_untrust_ip
     global fw_ip
+    global instance_id
     ip = ""
     u_ip = ""
-    postdata = json.load(open('./msg'))
-    #postdata = request.body.read()
-    print postdata
-    #data=json.loads(postdata)
-    data = postdata
+    postdata = request.body.read()
+    logger.info("POSTDATA {}".format(postdata))
+    data=json.loads(postdata)
+    logger.info("DATA {}".format(data))
+
+    ##SCALE OUT
     if 'operation' in data and data['operation'] == 'Scale Out':
        resource_id = data['context']['resourceId']
        rg_name = data['context']['resourceGroupName']
@@ -189,69 +197,75 @@ def main():
        args = 'az vmss list-instances --ids '+resource_id
        x = json.loads(subprocess.check_output(shlex.split(args)))
        for i in x:
-           instance_id.append(i['instanceId'])
-           args = 'az vmss nic list-vm-nics --resource-group ' + rg_name + ' --vmss-name ' + vmss_name + ' --instance-id ' +  instance_id
-           y = json.loads(subprocess.check_output(shlex.split(args)))
-           fw_ip.append(y[0]['ipConfigurations'][0]['privateIpAddress'])
-           fw_untrust_ip.append(y[0]['ipConfigurations'][1]['privateIpAddress'])
-    
-    # Now that we have all the ip addresses, time to poll each one and see which one is the scaled out fw 
-    for ip in fw_ip and u_ip in fw_untrust_ip:
-        cmd = "https://"+ip+"/api/?type=op&cmd=<show><chassis-ready></chassis-ready></show>&key="+api_key
-        logger.info('[INFO]: Sending command: %s', cmd)
-        try:
-           response = urllib2.urlopen(cmd, context=gcontext, timeout=10).read()
-        except socket.timeout, e:
-            logger.info("[INFO]: No response from FW. So this is most likely the fw that got scaled up! {}".format(e))
-            scaled_fw_ip = ip
-            fw_untrust_ip = u_ip
-            break #we got it.
-        else:
-            logger.info("This fw is up...so move on")
-    
-    err = 'no'
-    while (True):
-        err = check_auto_commit_status(scaled_fw_ip)
-        if err == 'yes':
-            break
-        else:
-          time.sleep(10)
-          continue
+           if i['instanceId'] not in instance_list:
+               instance_id = i['instanceId']
+               logger.info("[INFO]: Instance ID: {}".format(instance_id))
+               args = 'az vmss nic list-vm-nics --resource-group ' + rg_name + ' --vmss-name ' + vmss_name + ' --instance-id ' +  instance_id
+               y = json.loads(subprocess.check_output(shlex.split(args)))
+               instance_list[instance_id].append({'mgmt-ip': y[0]['ipConfigurations'][0]['privateIpAddress']})
+               instance_list[instance_id].append({'untrust-ip': y[0]['ipConfigurations'][1]['privateIpAddress']})
+               logger.info("[INFO]: Instance ID: {}".format(instance_list[instance_id]['mgmt-ip']))
+               logger.info("[INFO]: Instance ID: {}".format(instance_list[instance_id]['untrust-ip']))
+           else:
+               continue 
+       scaled_fw_ip = instance_list[instance_id]['mgmt-ip']
+       scaled_fw_untrust_ip = instance_list[instance_id]['untrust-ip']
+       err = 'no'
+       while (True):
+           err = check_auto_commit_status(scaled_fw_ip)
+           if err == 'yes':
+               break
+           else:
+               time.sleep(10)
+               continue
 
-    while (True):
-        err = check_fw_up(scaled_fw_ip)
-        if err == 'yes':
-            break
-        else:
-            time.sleep(10)
-            continue
+       while (True):
+          err = check_fw_up(scaled_fw_ip)
+          if err == 'yes':
+              break
+          else:
+              time.sleep(10)
+              continue
 
-    #PUSH NAT RULE OR UPDATE THE NAT ADDRESS OBJECTS
-    cmd="https://"+scaled_fw_ip+"/api/?type=config&action=set&key="+api_key+"&xpath=/config/devices/entry/vsys/entry/address&element=<entry%20name='AWS-NAT-ILB'><description>ILB-IP-address</description><ip-netmask>"+ilb_ip+"</ip-netmask></entry>"
-    logger.info("[INFO]: Pushing ILB NAT RULE")
-    try:
-        response = urllib2.urlopen(cmd, context=gcontext, timeout=5).read()
-    except Exception as e:
-         logger.info("[INFO]: Push NAT Address reponse: {}".format(e))
-         sys.exit(0)
+       #PUSH NAT RULE OR UPDATE THE NAT ADDRESS OBJECTS
+       cmd="https://"+scaled_fw_ip+"/api/?type=config&action=set&key="+api_key+"&xpath=/config/devices/entry/vsys/entry/address&element=<entry%20name='AWS-NAT-ILB'><description>ILB-IP-address</description><ip-netmask>"+ilb_ip+"</ip-netmask></entry>"
+       logger.info("[INFO]: Pushing ILB NAT RULE")
+       try:
+            response = urllib2.urlopen(cmd, context=gcontext, timeout=5).read()
+       except Exception as e:
+            logger.info("[INFO]: Push NAT Address reponse: {}".format(e))
+            sys.exit(0)
          
-    cmd="https://"+scaled_fw_ip+"/api/?type=config&action=set&key="+api_key+"&xpath=/config/devices/entry/vsys/entry/address&element=<entry%20name='AWS-NAT-UNTRUST'><description>UNTRUST-IP-address</description><ip-netmask>"+scaled_fw_untrust_ip+"</ip-netmask></entry>"
-    logger.info("[INFO]: Updating Untrust ip address for NAT rule")
-    try:
-        response = urllib2.urlopen(cmd, context=gcontext, timeout=5).read()
-    except Exception as e:
-         #logger.error("[NAT Address RESPONSE]: {}".format(e))
-         logger.info("[INFO]: Untrust object update response: {}".format(e))
-         sys.exit(0)
+       cmd="https://"+scaled_fw_ip+"/api/?type=config&action=set&key="+api_key+"&xpath=/config/devices/entry/vsys/entry/address&element=<entry%20name='AWS-NAT-UNTRUST'><description>UNTRUST-IP-address</description><ip-netmask>"+scaled_fw_untrust_ip+"</ip-netmask></entry>"
+       logger.info("[INFO]: Updating Untrust ip address for NAT rule")
+       try:
+            response = urllib2.urlopen(cmd, context=gcontext, timeout=5).read()
+       except Exception as e:
+            #logger.error("[NAT Address RESPONSE]: {}".format(e))
+            logger.info("[INFO]: Untrust object update response: {}".format(e))
+            sys.exit(0)
 
-    cmd="https://"+scaled_fw_ip+"/api/?type=commit&cmd=<commit></commit>&key="+api_key
-    try:
-        response = urllib2.urlopen(cmd, context=gcontext, timeout=5).read()
-    except Exception as e:
-         logger.info("[ERROR]: Commit error: {}".format(e))
-         sys.exit(0)
+       cmd="https://"+scaled_fw_ip+"/api/?type=commit&cmd=<commit></commit>&key="+api_key
+       try:
+            response = urllib2.urlopen(cmd, context=gcontext, timeout=5).read()
+       except Exception as e:
+            logger.info("[ERROR]: Commit error: {}".format(e))
+            sys.exit(0)
     
-    return "<h1>Hello World!</h1>"
-if __name__ == "__main__":
-   main()
+       return "<h1>Hello World!</h1>"
+    else:
+       ##SCALE IN
+       if 'operation' in data and data['operation'] == 'Scale In':
+        resource_id = data['context']['resourceId']
+        rg_name = data['context']['resourceGroupName']
+        vmss_name = data['context']['resourceName'] 
+        args = 'az vmss list-instances --ids '+resource_id
+        x = json.loads(subprocess.check_output(shlex.split(args)))
+        ##NEED TO REMOVE THE INSTANCE THAT GOT SCALED IN...HOW TO FIGURE THAT OUT?
+        ##SO WHAT DOES THE vmss cli return?
+    return "<h1>Bye Bye World!</h1>"
+
+    # The following lines will call the BottleDaemon script and launch a daemon in the background.
+    if __name__ == "__main__":
+      daemon_run(host='0.0.0.0', port=80)
 #run(host='0.0.0.0', port=80, debug=True)
