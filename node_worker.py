5f0c48bdad81ad7f7654567eddf317b37d9c47e8
Narayan Iyengar <niyengar@paloaltonetworks.com>
2018-04-11 14:17:44 -0700

    minor logging error fixed

--- node_worker.py
+++ node_worker.py
@@ -317,7 +317,7 @@ def main():
         args = 'az resource show -g ' + rg_name + ' --resource-type microsoft.insights/components -n ' + appinsights_name + ' --query "properties.InstrumentationKey"'
         logger.info("[INFO]: Seding az login command {}".format(args))
         instrumentation_key = subprocess.check_output(shlex.split(args))
-        logger.info("[INFO]: output of az login {}".format(instrumentation_key))
+        logger.info("[INFO]: output of az resource show {}".format(instrumentation_key))
         tc = TelemetryClient(instrumentation_key)
         for metric in metric_list:
             tc.track_metric(metric, 0)
