43d1e8ce6a62d86a529ef7f59cca96a7f71d6d80
Narayan Iyengar <niyengar@paloaltonetworks.com>
2018-04-12 12:45:36 -0700

    use a shell script to invoke worker script

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get update
 apt-get install -y python-pip
-pip install azure-cli bottle bottledaemon applicationinsight
+pip install azure-cli bottle bottledaemon applicationinsights
 python ./node_worker.py $1 $2 $3 $4 $5 $6 $7 &
