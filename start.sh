c08c2b77ecd365dc899b48ec89c1d128570120bb
Narayan Iyengar <niyengar@paloaltonetworks.com>
2018-04-12 13:53:42 -0700

    debug

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 apt-get update
 apt-get install -y python-pip
 pip install azure-cli bottle bottledaemon applicationinsights
-python ./node_worker.py $1 $2 $3 $4 $5 $6 $7 &
+./node_worker.py $1 $2 $3 $4 $5 $6 $7 
