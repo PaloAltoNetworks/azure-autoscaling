1d8b882663c7abfaa3997f88a182e59b3eaafe17
Jason Meurer <30842226+jasonmeurer@users.noreply.github.com>
2018-04-18 14:21:22 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt-get update
-apt-get install -y python-pip
-pip install azure-cli applicationinsights
-./publish.py $1 $2 $3 $4 $5 $6 $7
+apt-get update &&
+apt-get install -y python-pip &&
+pip install azure-cli applicationinsights &&
+./publish.py $1 $2 $3 $4 $5 $6 $7 &&
 ./node_worker.py $1 $2 $3 $4 $5 $6 $7
